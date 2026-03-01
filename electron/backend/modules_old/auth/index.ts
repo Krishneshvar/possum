edff8dd6e8ac4385674c10a821b485755388e8d8
Krishneshvar <krishjn07@gmail.com>
2026-03-01 11:09:47 +0530

    chore: delete refactored modules

--- electron/backend/modules_old/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as authRoutes } from './auth.routes.js';
-export * as AuthService from './auth.service.js';
-export * as SessionRepository from './session.repository.js';
-export { authEvents } from './auth.events.js';
-export type { AuthUser, LoginResponse, SessionData } from './auth.types.js';
